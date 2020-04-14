77aa6cdb2b3ac788f3eb751d1ea746d96d2278f8
mabinewb <user@khu.ac.kr>
2020-04-15 04:27:55 +0900

    북마크 등록 방법 수정

--- pdfix.js
+++ pdfix.js
@@ -7,7 +7,7 @@ window.onload = function () {
 // Next/previous controls
 function plusSlides(n) {
     if(n == 1 && slideIndex == 10 && document.querySelector("#api_key").value.length == 0){
-        alert("다음 단계로 넘어가기 전에 입력칸에 API 키를 입력해주세요!");
+        alert("다음 단계로 넘어가기 전에 아래 입력칸에 생성된 API 키를 입력해주세요!");
     } else showSlides(slideIndex += n);
 }
 
@@ -34,7 +34,7 @@ function showSlides(n) {
     if(n == 1){
         document.getElementsByClassName("prev")[0].style.display = "none";
         document.getElementsByClassName("next")[0].style.display = "block";
-    } else if(n == 14){
+    } else if(n == 12){
         document.getElementsByClassName("prev")[0].style.display = "block";
         document.getElementsByClassName("next")[0].style.display = "none";
     } else {
@@ -45,16 +45,16 @@ function showSlides(n) {
 
 function setAPIkey() {
     var api_key = document.querySelector("#api_key").value.trim();
-    document.querySelector("#fix_url").value = "javascript:gapi.client.setApiKey(\"" + api_key + "\"); gapi.client.load(\"youtube\", \"v3\");alert(\"플디픽스 적용 완료\");";
+    document.querySelector("#fix_link").href = "javascript:if(!window.location.href.includes(\"://plug.dj/\") || typeof gapi == \"undefined\"){alert(\"plug.dj 음악 감상 페이지 내에서 사용하세요.\");} else {gapi.client.setApiKey(\"" + api_key + "\"); gapi.client.load(\"youtube\", \"v3\"); alert(\"플디픽스 적용 성공!!\");}";
 }
 
-function copy(){
-    if(document.querySelector("#fix_url").textLength == 0){
-        alert("복사할 내용이 없습니다.");
-    } else {
-        document.querySelector("#fix_url").select();
-        document.querySelector("#fix_url").focus();
-        document.execCommand("copy");
-        alert("URL 복사 완료");
-    }
-};
+// function copy(){
+//     if(document.querySelector("#fix_url").textLength == 0){
+//         alert("복사할 내용이 없습니다.");
+//     } else {
+//         document.querySelector("#fix_url").select();
+//         document.querySelector("#fix_url").focus();
+//         document.execCommand("copy");
+//         alert("URL 복사 완료");
+//     }
+// }
