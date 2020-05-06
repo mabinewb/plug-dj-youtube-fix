2e729166e273a6f488ef1de5f3ec54479b3d10c2
mabinewb <user@khu.ac.kr>
2020-05-06 19:03:56 +0900

    API키 유효한지 확인하는 코드 추가

--- pdfix.js
+++ pdfix.js
@@ -6,9 +6,29 @@ window.onload = function () {
 
 // Next/previous controls
 function plusSlides(n) {
-    if(n == 1 && slideIndex == 10 && document.querySelector("#api_key").value.length == 0){
-        alert("다음 단계로 넘어가기 전에 아래 입력칸에 생성된 API 키를 입력해주세요!");
-    } else showSlides(slideIndex += n);
+    if (n == 1 && slideIndex == 10 && document.querySelector("#api_key").value.length == 0) {
+        alert("다음 단계로 넘어가기 전에 생성된 API 키를 입력해주세요!");
+    } else if (n == 1 && slideIndex == 10) {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState == this.DONE) {
+                if (request.status === 200) {
+                    alert("입력된 API키가 유효합니다.");
+                    showSlides(slideIndex += n);
+                } else if (request.status === 400) {
+                    var response = JSON.parse(request.responseText);
+                    if (response.error)
+                        alert("입력된 API키가 유효하지 않습니다. (오류 메시지 : " + JSON.stringify(response.error.errors[0]) + ")");
+                } else {
+                    alert("기타 오류 발생");
+                }
+            }
+        };
+        request.open("GET", "https://www.googleapis.com/youtube/v3/search?part=id&key=" + document.querySelector("#api_key").value.trim(), true);
+        request.send();
+    } else {
+        showSlides(slideIndex += n);
+    }
 }
 
 // Thumbnail image controls
@@ -31,10 +51,10 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 
-    if(n == 1){
+    if (n == 1) {
         document.getElementsByClassName("prev")[0].style.display = "none";
         document.getElementsByClassName("next")[0].style.display = "block";
-    } else if(n == 12){
+    } else if (n == 12) {
         document.getElementsByClassName("prev")[0].style.display = "block";
         document.getElementsByClassName("next")[0].style.display = "none";
     } else {
@@ -47,14 +67,3 @@ function setAPIkey() {
     var api_key = document.querySelector("#api_key").value.trim();
     document.querySelector("#fix_link").href = "javascript:if(!window.location.href.includes(\"://plug.dj/\") || typeof gapi == \"undefined\"){alert(\"plug.dj 음악 감상 페이지 내에서 사용하세요.\");} else {gapi.client.setApiKey(\"" + api_key + "\"); gapi.client.load(\"youtube\", \"v3\"); alert(\"플디픽스 적용 성공!!\");}";
 }
-
-// function copy(){
-//     if(document.querySelector("#fix_url").textLength == 0){
-//         alert("복사할 내용이 없습니다.");
-//     } else {
-//         document.querySelector("#fix_url").select();
-//         document.querySelector("#fix_url").focus();
-//         document.execCommand("copy");
-//         alert("URL 복사 완료");
-//     }
-// }
